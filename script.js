50db8134326374aca22bfae0643f29f398c6e8a6
Darielton Silva <139516266+Darieltonss@users.noreply.github.com>
2023-10-13 21:04:14 -0300

    primeiro commit na branch main

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#botao-cadastrar").click(function() {
+      $("#form-cadastrar").slideToggle("slow");
+      $("#section-login").slideToggle("slow");
+  
+      $("#botao-logar").show();
+      $("#botao-cadastrar").hide();
+    });
+  
+    $("#botao-logar").click(function() {
+      $("#form-cadastrar").slideToggle("slow");
+      $("#section-login").slideToggle("slow");
+  
+      $("#botao-cadastrar").show();
+      $("#botao-logar").hide();
+    });
+  });
